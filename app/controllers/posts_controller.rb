72eca4a51c1362915531e7572a98f89e07796195
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-08-18 21:58:00 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,6 @@ class PostsController < ApplicationController
 	end
 
 	def update
-
 	  @post = Post.find(params[:id])
 	  @post.update(params.require(:post).permit(:title, :description))
 	  redirect_to post_path(@post)
